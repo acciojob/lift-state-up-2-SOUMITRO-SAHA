407894f2cca452010ecc2b0179088e307547babb
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-07-28 23:09:51 +0530

    Update 1

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Child = ({ setInputValue }) => {
 	return (
-		<div>
+		<div className='child'>
 			<h1>Child Component</h1>
 			<input type='text' onChange={(e) => setInputValue(e.target.value)} />
 		</div>
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -5,11 +5,11 @@ const Parent = () => {
 	const [inputValue, setInputValue] = useState("");
 
 	return (
-		<>
+		<div className='parent'>
 			<h1>Parent Component</h1>
 			<p>{inputValue}</p>
 			<Child setInputValue={setInputValue} />
-		</>
+		</div>
 	);
 };
 
